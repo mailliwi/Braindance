d3d3891e39a0fdce3b5eee9f9a68ec4b495f38c1
William Dupont <williamd.contact@gmail.com>
2022-10-03 15:50:39 +0100

    Initial Commit

---
+++ Braindance/BraindanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  BraindanceApp.swift
+//  Braindance
+//
+//  Created by William Dupont on 03/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct BraindanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
